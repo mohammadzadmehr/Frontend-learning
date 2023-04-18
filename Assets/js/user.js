32f76bce38689949fa07bd85f4c57bed6e5e3e47
mohammad <mohammadzadmehrofficial@gmail.com>
2023-04-19 01:38:30 +0430

    Part #10

--- Assets/js/user.js
+++ Assets/js/user.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+const addUserButton=document.getElementById("add_user_button");
+addUserButton.addEventListener('click',()=>{
+  document.querySelector('.add_user_modal').classList.add('show')
+  document.querySelector('.add_user_modal_back').classList.remove('dis-non')
+})
+
+
+
   });
